778db16019b9beaa4bc1d1b61802622c5b9ab5a6
root <root@AB9EFA7>
2021-04-29 13:42:00 +0000

    rework

--- src/RedisService.php
+++ src/RedisService.php
@@ -1,60 +1,61 @@
 <?php
+
 namespace PhpNamespacedRedisClient;
 
 class RedisService
 {
-  public function __call($methodName, $args) {
-    call_user_func_array(array($this->getRedisClient(), $methodName), $args);
-  }
-  
-  public function __construct()
-  {
-  }
-  
-  public function setRedisClient($val)
-  {
-    $this->redisClient = $val;
-  }
-  
-  protected function getRedisClient()
-  {
-    return $this->redisClient;
-  }
-  
-  
-  public function setNamespace($val)
-  {
-    $this->myNamespace = $val;  
-  }
-  
+    public function __call($methodName, $args)
+    {
+        call_user_func_array(array($this->getRedisClient(), $methodName), $args);
+    }
 
-  protected function getNamespace()
-  {
-    return $this->myNamespace;
-  }
-  
-  protected function prefixWithNamespace($string)
-  {
-    return $this->getNamespace().'_'.$this->getNamespace().'_'.$string;  
-  }
-  
-  public function get($key)
-  {
-    $prefixedKey = $this->prefixWithNamespace($key);
-    return $this->getRedisClient()->get($prefixedKey);
-  }
+    public function __construct()
+    {
+    }
 
-  public function set($key, $value)
-  {
-    $prefixedKey = $this->prefixWithNamespace($key);
-    return $this->getRedisClient()->set($prefixedKey, $value);
-  }
-  
-  public function exists($key)
-  {
-    $prefixedKey = $this->prefixWithNamespace($key);
-    return $this->getRedisClient()->exists($prefixedKey);
-  }
-  
-  
+    public function setRedisClient($val)
+    {
+        $this->redisClient = $val;
+    }
+
+    protected function getRedisClient()
+    {
+        return $this->redisClient;
+    }
+
+    public function setNamespace($val)
+    {
+        $this->myNamespace = $val;
+    }
+
+    protected function getNamespace()
+    {
+        return $this->myNamespace;
+    }
+
+    protected function prefixWithNamespace($string)
+    {
+        return $this->getNamespace().'_'.$this->getNamespace().'_'.$string;
+    }
+
+    public function get($key)
+    {
+        $prefixedKey = $this->prefixWithNamespace($key);
+
+        return $this->getRedisClient()->get($prefixedKey);
+    }
+
+    public function set($key, $value)
+    {
+        $prefixedKey = $this->prefixWithNamespace($key);
+
+        return $this->getRedisClient()->set($prefixedKey, $value);
+    }
+
+    public function exists($key)
+    {
+        $prefixedKey = $this->prefixWithNamespace($key);
+
+        return $this->getRedisClient()->exists($prefixedKey);
+    }
 }
